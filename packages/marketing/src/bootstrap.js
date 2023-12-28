82fb0f64e72aa23ed48d7ba13dc967c6ce30b729
Marcos Rodriguez <marcossalvo95@gmail.com>
2023-12-28 19:28:39 +0100

    feat: :sparkles: Updating marketing router when navigating inside host app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -12,6 +12,13 @@ const mount = (el, { onNavigate }) => {
         <App history={history} />,
         el
     );
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) history.push(nextPathname);
+        }
+    }
 }
 
 if (process.env.NODE_ENV === 'development') {
