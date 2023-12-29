125843afb1f8ca04edb8db4be9e5b7cf661d5173
Marcos Rodriguez <marcossalvo95@gmail.com>
2023-12-29 20:20:17 +0100

    feat: :sparkles: Receive initial pathname inside auth remote when mounting

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -7,13 +7,16 @@ export default () => {
     const history = useHistory();
 
     useEffect(() => {
-        const { onParentNavigate } = mount(ref.current, {
-            onNavigate: ({ pathname: nextPathname }) => {
-                const { pathname } = history.location;
-                if (pathname !== nextPathname) history.push(nextPathname);
-                
+        const { onParentNavigate } = mount(
+            ref.current, 
+            {
+                initialPath: history.location.pathname,
+                onNavigate: ({ pathname: nextPathname }) => {
+                    const { pathname } = history.location;
+                    if (pathname !== nextPathname) history.push(nextPathname);
+                }
             },
-        });
+        );
 
         history.listen(onParentNavigate);
     }, []);
