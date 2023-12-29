33f86139988b2fc40bf4208d2fc4b2401cff6261
Marcos Rodriguez <marcossalvo95@gmail.com>
2023-12-29 19:37:32 +0100

    feat: :sparkles: Create auth remote

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
 const generateClassName = createGenerateClassName({
     productionPrefix: 'au',
 });
@@ -12,6 +15,8 @@ export default ({ history }) => {
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history} >
                     <Switch>
+                        <Route exact path="/auth/signin" component={Signin} />
+                        <Route exact path="/auth/signup" component={Signup} />
                     </Switch>
                 </Router>
             </StylesProvider>
