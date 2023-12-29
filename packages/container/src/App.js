1717076a794cf456a305a797bf10655c923ab636
Marcos Rodriguez <marcossalvo95@gmail.com>
2023-12-29 20:35:07 +0100

    feat: :sparkles: Lazy load remotes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
-import AuthApp from "./components/AuthApp";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+
+const MarketingAppLazy = lazy(() => import("./components/MarketingApp"));
+const AuthAppLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co",
@@ -15,10 +16,12 @@ export default () => {
             <BrowserRouter>
                 <div>
                     <Header />
-                    <Switch>
-                        <Route path="/auth" component={AuthApp} />
-                        <Route path="/" component={MarketingApp} />
-                    </Switch>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path="/auth" component={AuthAppLazy} />
+                            <Route path="/" component={MarketingAppLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </BrowserRouter>
         </StylesProvider>
